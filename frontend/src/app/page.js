8d6f1b3f328d19aa3967d5b09ad79730c0812e6b
Ankit Kumain <kumankit10@gmail.com>
2024-01-20 16:41:51 +0530

    feat: colour palette frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,30 +1,47 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [prompt, setPrompt] = useState("");
+  const [colours, setColours] = useState(["#FFFEee", "#FFF", "#FFF", "#FFF"]);
+
+  async function getColourPalette(){
+    try{
+      const response = await fetch("http://localhost:3000/colour-palette", {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({prompt}), 
+      });
+      const colours = await response.json();
+      setColours(colours.colors);
+      setPrompt("");
+    }catch{
+      console.log("Something went wrong!");
+    }
+    
+  }
+
   return (
     <div>
-     
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-row w-screen relative">
+
+      {
+        colours.map(colour=>{
+          return <div style={{height:"100vh",backgroundColor:colour}} className="grow"></div>
+        })
+      }
       
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-     
-      </div>
-
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-     
-      </div>
+      
+     <div className="flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <input type="text" maxLength="500" value={prompt} onChange={(e)=>setPrompt(e.target.value)} required placeholder="Tropical sunset"/>
+      <button className="bt" onClick={getColourPalette}>Generate</button>
+     </div>
+   
     </main>
     </div>
   );
